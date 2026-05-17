c8e744d8ecd6312e022bb755056ad04faf2228a1
rushikeshnarute123 <rushikeshnarute3135@gmail.com>
2026-05-17 16:51:19 +0530

    a

--- react-states-class/src/App.jsx
+++ react-states-class/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Counter from './Counter';
+import Like from './Like';
 
 function App() {
   return (
     <>
       <h1>States in React</h1>
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <Like></Like>
     </>
   );
 }
