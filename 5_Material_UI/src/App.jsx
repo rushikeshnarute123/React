dc2e15686178c26d12334e978dae5d584073f651
rushikeshnarute123 <rushikeshnarute3135@gmail.com>
2026-07-23 14:47:30 +0530

    a

---
+++ 5_Material_UI/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  )
+}
+
+export default App
