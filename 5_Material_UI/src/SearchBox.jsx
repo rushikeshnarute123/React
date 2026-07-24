96fe50f3b8cc28c9d248cfaadb5824ff3f8edefc
rushikeshnarute123 <rushikeshnarute3135@gmail.com>
2026-07-24 17:47:53 +0530

    a

---
+++ 5_Material_UI/src/SearchBox.jsx
@@ -0,0 +1,32 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import "./SearchBox.css"
+
+
+
+export default function SearchBox() {
+    let [city, setCity] = useState("");
+
+    let handleChange = (event) => {
+        setCity(event.target.value);
+    };
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+    };
+
+
+    return (
+        <div className="SearchBox">
+            <h3>Search for the weather</h3>
+            <form onSubmit={handleSubmit}>
+                <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
+                <br /><br />
+                <Button variant="contained" type="submit">Send</Button>
+            </form>
+        </div>
+    )
+}
